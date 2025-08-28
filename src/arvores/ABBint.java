4e91b7b02601a1c5aa45e603ce8cea3bfb7d393e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-27 22:35:30 -0300

    Assunto 11 - Implementação ABB

---
+++ src/arvores/ABBint.java
@@ -0,0 +1,36 @@
+package arvores;
+
+public class ABBint {
+
+    private class NO {
+        int dado;
+        NO esq;
+        NO dir;
+    }
+
+    public NO root = null;
+
+    public NO inserir(NO p, int info) {
+        if(p == null) {
+            p = new NO();
+            p.dado = info;
+            p.esq = null;
+            p.dir = null;
+        }
+        else if (info < p.dado) {
+            p.esq = inserir(p.esq, info);
+        }
+        else {
+            p.dir = inserir(p.dir, info);
+        }
+        return p;
+    }
+
+    public void show(NO p){
+        if (p != null) {
+            show(p.esq);
+            System.out.println(p.dado);
+            show(p.dir);
+        }
+    }
+}
